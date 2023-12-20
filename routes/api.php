ef15b364b4d98a340ac2ac88615ad05243e420a2
Krishna Tiwari <kisnatwari@gmail.com>
2023-12-20 21:29:06 +0545

    deleted unnecessary views and prepared route for post

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,6 +26,7 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware("auth:sanctum")->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::apiResource("post", PostController::class);
 });
 
 
