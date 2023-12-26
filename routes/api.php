7bdf84b7716640a000ffcd1dc77a1fd0e6d9075e
Krishna Tiwari <kisnatwari@gmail.com>
2023-12-26 12:17:38 +0545

    csrf config and api reconfigured

--- routes/api.php
+++ routes/api.php
@@ -6,24 +6,13 @@ use App\Http\Controllers\API\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
-Route::post('/register', [AuthController::class,'register']);
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class,'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
 
 //private routes
 Route::middleware("auth:sanctum")->group(function(){
@@ -35,7 +24,3 @@ Route::middleware("auth:sanctum")->group(function(){
 //public routes
 Route::apiResource("post", PostController::class)->only(["index", "show"]);
 Route::apiResource("comment", CommentController::class)->only(["index", "show"]);
-
-
-
-Route::post('/demo', [AuthController::class, 'demo']);
